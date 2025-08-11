f64b9ebe777eb2c159d8d483a324480822a9b57a
Rishi Lokesh <rishilokesh07@gmail.com>
2025-08-10 22:32:58 -0400

    cleaned up imports

--- include/AlgoTradeV2/fxData.hpp
+++ include/AlgoTradeV2/fxData.hpp
@@ -1,12 +1,6 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
-#include <format>
 #include "marketData.hpp"
-#include "../httplib/httplib.h"
 #include <string>
-#include <sstream>
-#include "../json/json.hpp"
-#include "timeFunctions.hpp"
-
 
 
 class fxData: public marketData{
